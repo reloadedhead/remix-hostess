1dd58ac74956179f29175cddb1a93fb2521bb04f
Tomás García Gobet <tomasgobet@icloud.com>
2022-11-05 10:18:07 +0100

    adds disabled state to button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,28 @@
 import type { HTMLProps, ReactNode } from "react";
+import cn from "classnames";
 
 type Props = HTMLProps<HTMLButtonElement> & {
   startIcon?: ReactNode;
 };
 
-export default function Button(props: Props) {
+export default function Button({
+  disabled,
+  startIcon,
+  children,
+  ...rest
+}: Props) {
   return (
     <button
-      {...props}
-      className="rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500"
+      {...rest}
+      disabled={disabled}
+      className={cn(
+        "rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500",
+        disabled ? "cursor-not-allowed opacity-50" : null
+      )}
     >
       <div className="flex space-x-2">
-        {props.startIcon}
-        <span>{props.children}</span>
+        {startIcon}
+        <span>{children}</span>
       </div>
     </button>
   );
